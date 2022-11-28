48b092e7ee537e8097a25f6587c244df132ff673
Astric215 <davidiwd215@gmail.com>
2022-11-28 12:28:31 -0800

    minor fixes

--- Assets/ScriptableObjects/Card.cs
+++ Assets/ScriptableObjects/Card.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class Card : ScriptableObject
 {
     public string word;
--- Assets/ScriptableObjects/Character.cs
+++ Assets/ScriptableObjects/Character.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Character", menuName = "Character")]
 public class Character : ScriptableObject
 {
-    public string name;
-    public string description;
+    public string charName;
     public bool stunned;
     public Sprite artwork;
 
     public int speed;
     public int dmgToStun;
+    public int weakness;
+
+    public List<Card> deck;
 }
