e0f61f6aaf640be54c063e2b48b7e036b5fa88ee
Voads <blue.j.row@gmail.com>
2022-11-29 17:46:05 -0800

    Updated card sprites, and file names in card.cs

--- Assets/ScriptableObjects/Card.cs
+++ Assets/ScriptableObjects/Card.cs
@@ -11,28 +11,28 @@ public class Card : ScriptableObject
     public int damageLevel = 1;
     public int rhyme = 1;
     public string text;
-
+    
     public void getSprite ()
     {
         switch (damageType) 
         {
             case 1:
-                background = Resources.Load<Sprite>("Sprites/FireC_S 1");
+                background = Resources.Load<Sprite>("Sprites/Card_Fire 1");
                 break;
             case 2:
-                background = Resources.Load<Sprite>("Sprites/IceC_S");
+                background = Resources.Load<Sprite>("Sprites/Card_Ice");
                 break;
             case 3:
-                background = Resources.Load<Sprite>("Sprites/ProtecC_S");
+                background = Resources.Load<Sprite>("Sprites/Card_Protect");
                 break;
             case 4:
-                background = Resources.Load<Sprite>("Sprites/ShockC_S");
+                background = Resources.Load<Sprite>("Sprites/Card_Shock");
                 break;
             case 5:
-                background = Resources.Load<Sprite>("Sprites/ToxicC_S");
+                background = Resources.Load<Sprite>("Sprites/Card_Toxic");
                 break;
             default:
-                background = Resources.Load<Sprite>("Sprites/NeutralC_S 1");
+                background = Resources.Load<Sprite>("Sprites/Card_Neutral 1");
                 break;
         }
 
